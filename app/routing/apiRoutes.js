2eacba7f754bef6d410a4fe99f771c92553ef619
derek-haworth <derekhaw1@gmail.com>
2018-02-24 14:46:06 -0600

    set up routes for html and apis

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,9 @@
+// API Routes
+// =============================================================
+app.get('/api/friends', function(req, res) {
+	
+});
+
+app.post('/api/friends', function(req, res) {
+
+});
