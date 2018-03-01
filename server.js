335689c6dd097f7fd085502277fe435879d4e64b
derek-haworth <derekhaw1@gmail.com>
2018-02-28 21:54:31 -0600

    add routing

--- server.js
+++ server.js
@@ -12,6 +12,16 @@ var PORT = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+
+// Static files
+app.use(express.static('app/public'));
+
+
+// Router
+require('./app/routing/apiRoutes.js')(app);
+require('./app/routing/htmlRoutes.js')(app);
+
+
 app.listen(PORT, function() {
     console.log("I\'m listening... on port " + PORT);
 })
