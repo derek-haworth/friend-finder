4cb172ff16126a7c6d524d83992d2cdc10d867ba
derek-haworth <derekhaw1@gmail.com>
2018-03-02 22:07:05 -0600

    export routes

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,9 +1,16 @@
 // API Routes
 // =============================================================
-app.get('/api/friends', function(req, res) {
-	
-});
 
-app.post('/api/friends', function(req, res) {
+// Using fs to Read Data
+// linking our routes to read a series of data sources.
+var fs = require('fs');
 
-});
+module.exports = function(app, path) {
+	app.get('/api/friends', function(req, res) {
+		
+	});
+
+	app.post('/api/friends', function(req, res) {
+
+	});
+}
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,9 +1,12 @@
-// Routes
+// HTML Routes
 // =============================================================
-app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "home.html"));
-});
 
-app.get("/survey", function(req, res) {
-  res.sendFile(path.join(__dirname, "survey.html"));
-});
+module.exports = function(app, path) {
+	app.get("/", function(req, res) {
+	  res.sendFile(path.join(__dirname, "../public/home.html"));
+	});
+
+	app.get("/survey", function(req, res) {
+	  res.sendFile(path.join(__dirname, "../public/survey.html"));
+	});
+}
